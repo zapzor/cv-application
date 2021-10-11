9b1bd0635fd794158b1619a755d6f578b33411d7
Zapzor <imp.4xlbm@aleeas.com>
2021-10-11 05:41:24 +0100

    Add github pages

--- src/components/form/Results.js
+++ src/components/form/Results.js
@@ -6,20 +6,23 @@ class Results extends Component {
 
     return (
       <div className={control}>
-        <div>First name: {this.props.firstName}</div>
-        <div>Last name:{this.props.lastName}</div>
-        <div>Title: {this.props.title}</div>
-        <div>Email: {this.props.email}</div>
-        <div>Phone Number: {this.props.phoneNumber}</div>
-        <div>Qualifications:{this.props.qualifications}</div>
-        <div>University: {this.props.university}</div>
-        <div>From: {this.props.fromEducation}</div>
-        <div>Until: {this.props.untilEducation}</div>
-        <div>Company Name: {this.props.company}</div>
-        <div>Position Title: {this.props.position}</div>
-        <div>Main Activities: {this.props.activities}</div>
-        <div>From: {this.props.workFrom}</div>
-        <div>Until: {this.props.workUntil}</div>
+        <ul>
+          <li>First name: {this.props.firstName}</li>
+          <li>Last name:{this.props.lastName}</li>
+          <li>Title: {this.props.title}</li>
+          <li>Email: {this.props.email}</li>
+          <li>Phone Number: {this.props.phoneNumber}</li>
+          <li>Qualifications:{this.props.qualifications}</li>
+          <li>University: {this.props.university}</li>
+          <li>From: {this.props.fromEducation}</li>
+          <li>Until: {this.props.untilEducation}</li>
+          <li>Company Name: {this.props.company}</li>
+          <li>Position Title: {this.props.position}</li>
+          <li>Main Activities: {this.props.activities}</li>
+          <li>From: {this.props.workFrom}</li>
+          <li>Until: {this.props.workUntil}</li>
+        </ul>
+
         <button onClick={this.props.redoForm}>Redo</button>
       </div>
     );
